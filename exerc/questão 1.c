f4f3d78326047d46e7e270e7577933ccde26fa84
Victor-Hugo-Andrade <hugovictor4185@gmail.com>
2023-08-28 15:35:35 -0300

    comparadores

--- exerc/questão 1.c
+++ exerc/questão 1.c
@@ -2,21 +2,17 @@
 
 int main() {
     float num1, num2;
-
-    // Solicita que o usuário insira dois números
     printf("Digite dois numeros: ");
     scanf("%f %f", &num1, &num2);
 
-    // Realiza cálculos e imprime os resultados
     printf("Soma: %.2f\n", num1 + num2);
-    printf("Subtr: %.2f\n", num1 - num2);
+    printf("Subtra: %.2f\n", num1 - num2);
     printf("Multipli: %.2f\n", num1 * num2);
 
-    // Verifica se num2 é zero antes de realizar a divisão
     if (num2 != 0) {
         printf("Divisao: %.2f\n", num1 / num2);
     } else {
-        printf("Divisao por zero nao e permitida.\n");
+        printf("Divisao por zero não a permitida.\n");
     }
 
     return 0;
